6d46aa3df2768d22221206d3774dbf501ee32baa
sangjinsu <climbplant39@kakao.com>
2023-05-01 14:11:00 +0900

    :sparkles: telnet 으로 통신

--- 015_understanding-TCP-servers/02_read/main.go
+++ 015_understanding-TCP-servers/02_read/main.go
@@ -36,4 +36,5 @@ func handle(conn net.Conn) {
 	// we have an open stream connection
 	// how does the above reader know when it's done?
 	fmt.Println("Code got here.")
+	fmt.Println("telnet 으로 통신")
 }
